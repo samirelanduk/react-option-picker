3f7e61ddc3345cface25bb740ae130ac144da140
Sam Ireland <mail@samireland.com>
2023-06-01 20:52:44 +0100

    Basic options display

--- src/Select.jsx
+++ src/Select.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Select = props => {
 
   const {
+    value,
+    options,
     className,
     inputClassName,
+    optionsClassName,
   } = props;
 
+  const [text, setText] = useState("");
+  const [showOptions, setShowOptions] = useState(false);
+
   return (
-    <div className={className}>
+    <div className={className} style={{position: "relative"}}>
       <input
         className={inputClassName}
+        value={text}
+        onChange={e => setText(e.target.value)}
+        onClick={() => setShowOptions(true)}
       />
+      <div
+        className={optionsClassName}
+        style={{position: "absolute", display: showOptions ? undefined : "none"}}
+      >
+        {options.map(option => (
+          <div className="optionClassName">
+            {option}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
