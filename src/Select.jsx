f3faf68f80872db5a32296f0ba756398ebbfb517
Sam Ireland <mail@samireland.com>
2023-06-01 21:02:45 +0100

    Selected state

--- src/Select.jsx
+++ src/Select.jsx
@@ -4,30 +4,45 @@ const Select = props => {
 
   const {
     value,
+    setValue,
     options,
+    filter,
     className,
     inputClassName,
     optionsClassName,
   } = props;
 
-  const [text, setText] = useState("");
+  const [typedText, setTypedText] = useState(null);
   const [showOptions, setShowOptions] = useState(false);
+
+  const selectedOption = options.find(option => option.id === value);
+
+  const displayText = typedText || selectedOption?.label || "";
+
+  const optionClicked = option => {
+    setValue(option.id);
+    setShowOptions(false);
+  }
 
   return (
     <div className={className} style={{position: "relative"}}>
       <input
         className={inputClassName}
-        value={text}
-        onChange={e => setText(e.target.value)}
+        value={displayText}
+        onChange={e => filter && setText(e.target.value)}
         onClick={() => setShowOptions(true)}
       />
       <div
         className={optionsClassName}
         style={{position: "absolute", display: showOptions ? undefined : "none"}}
       >
-        {options.map(option => (
-          <div className="optionClassName">
-            {option}
+        {options.map((option, index) => (
+          <div
+            key={index}
+            className="optionClassName"
+            onClick={() => optionClicked(option)}
+          >
+            {option.label}
           </div>
         ))}
       </div>
